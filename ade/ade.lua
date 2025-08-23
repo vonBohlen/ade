d10a44029abb1d8de4b416fa21c0dc4af2d41b82
Justus Kagerer <justus-kagerer@web.de>
2025-08-23 13:37:21 +0200

    add basic POC for windows creation and image rendering

---
+++ ade/ade.lua
@@ -0,0 +1,15 @@
+function awm()
+  warning("awm starting...");
+
+  system_load("builtin/mouse.lua");
+  system_load("builtin/keyboard.lua");
+
+  initialize_desktop();
+end
+
+function initialize_desktop()
+  local bg = load_image("bg.jpg", 2, 1280, 720);
+  show_image(bg);
+
+
+end
